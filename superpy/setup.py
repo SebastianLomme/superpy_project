0975142e06b70bcf5a2157ac37199b630a393b87
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-12-02 18:26:05 +0000

    rich added

--- superpy/setup.py
+++ superpy/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='superpy',
     version='0.0.1',
-    packages=find_packages(),
+    packages=find_packages("rich"),
     entry_points={
         'console_scripts' : ['superpy=app.main:main']
     },
